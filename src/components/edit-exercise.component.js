e5cda65eb1f8f15cccc87e41cea1525c381a73b4
chtushar <choudharit23@gmail.com>
2020-04-17 23:03:43 +0530

    Basic Styling done. App is ready

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
+
+export default class EditExercises extends Component{
+    render(){
+        return(
+            <div>
+                <p>You are on the edit exercise Component</p>
+            </div>
+        )
+    }
+} 
