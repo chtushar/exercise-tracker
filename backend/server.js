d5e6a42b01ecc14c8740d5ceb01f76a0eb5864ef
chtushar <choudharit23@gmail.com>
2020-04-17 19:37:41 +0530

    Backend added

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+//FOR .env FILES
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//MIDDLEWARES
+app.use(cors()); 
+app.use(express.json()); //PARSE JSON
+
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
